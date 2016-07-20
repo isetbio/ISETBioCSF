a64b2a612a681c2103a69a0a9b64170a1e41a6e8
David Brainard <brainard@psych.upenn.edu>
2016-07-20 16:14:10 -0400

    And more.
    Signed-off-by: David Brainard <brainard@psych.upenn.edu>

--- toolbox/utility/IBIOColorDetectConfig.m
+++
@@ -1,9 +0,0 @@
-function IBIOColorDetectConfig
-% IBIOColorDetectConfig
-%
-% Configure things for working on the IBIOColorDetect project.
-
-% Add project toolbox to Matlab path
-AddToMatlabPathDynamically(fullfile(fileparts(which(mfilename)),'toolbox'));
-
-
