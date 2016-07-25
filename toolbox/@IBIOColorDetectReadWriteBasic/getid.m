59f5472bce6d4ebe96259ac1ec6e3ebba736f286
David Brainard <brainard@psych.upenn.edu>
2016-07-25 08:08:56 -0400

    Remove debris
    Signed-off-by: David Brainard <brainard@psych.upenn.edu>

--- toolbox/@IBIOColorDetectReadWriteBasic/getid.m
+++ toolbox/@IBIOColorDetectReadWriteBasic/getid.m
@@ -1,24 +1,96 @@
-function fileid = read(obj,name,varagin)
-% fileid = read(obj,name,varagin)
+function fileid = getid(obj,name,varagin)
+% fileid = getid(obj,name,varagin)
 %
-% Read data objects using metadata.
+% Get a unique id required for reading and writing a piece of data.
+% Here that is the full path to the file, but in a database world it could
+% be something else.
 %
 % Key/value pairs
 %   'Type'
-%     'mat' - Matlab .mat file (default)
+%     'mat' - Matlab .mat file
 %     'figure' - A figure
 %     'movie' - A movie
 %     'movieFile' - String with full path to temp movie file
 %   'ArtifactParams'
 %      Structure with artifact specific information.
+%   'FigureType'
+%      Type of figure to save, represented as file extension.
+%      'pdf' - PDF (default)
+%   'MovieType'
+%      Type for movie to save, represented as file extension.
+%      'm4v' - MPEG-4 (default)
 
-% Parse input.
+%% Parse input.
 p = inputParser;
 p.addRequired('name',@ischar);
+p.addRequired('data');
 p.addParameter('Type','mat',@ischar);
 p.addParameter('ArtifactParams','mat',@isstruct);
+p.addParameter('FigureType','pdf',@ischar);
+p.addParameter('MovieType','m4v',@ischar);
+p.parse(name,data,varargin{:});
 
-p.parse(params,name,varargin{:});
-name = p.Results.name;
+%% Get parent directory list and make sure the output tree is in place
+theParentDir = '';
+if (~isempty(obj.parentParamsList))
+    for ii = 1:length(obj.parentParamsList)
+        thisParentParams = obj.parentParamsList{ii};
+        switch(obj.parentParams.type)
+            case 'ResponseGeneration'
+                thisParentDir = obj.paramsToResponseGenerationDirName(thisParentParams);
+            case 'ColorModulation'
+                thisParentDir = obj.paramsToColorModulationDirName(thisParentParams);
+            otherwise
+                error('Unkown parent parameters type');
+        end
+        theParentDir = fullfile(theParentDir,thisParentDir);
+        if (~exist(theParentDir,'dir'))
+            mkdir(theParentDir);
+        end
+    end
+    theParentDir = fullfile(getpref('IBIOColorDetect','outputBaseDir'),theParentDir);
+else
+    theParentDir = getpref('IBIOColorDetect','outputBaseDir');
+    if (~exist(theParentDir,'dir'))
+        mkdir(theParentDir);
+    end
+end
+
+%% Get current dir name and make it if necessary
+theCurrentDir = theParentDir;
+for ii = 1:length(obj.currentParamsList)
+    thisCurrentParams = obj.currentParamsList{ii};
+    switch(thisCurrentParams.type)
+        case 'ResponseGeneration'
+            thisCurrentDir = obj.paramsToResponseGenerationDirName(thisCurrentParams);
+        case 'ColorModulation'
+            thisCurrentDir = obj.paramsToColorModulationDirName(thisCurrentParams);
+        otherwise
+            error('Unkown current parameters type');
+    end
+    theCurrentDir = fullfile(theCurrentDir,thisCurrentDir);
+    if (~exist(theCurrentDir,'dir'))
+        mkdir(theCurrentDir);
+    end
+end
+
+%% Compose directory where data should go
+switch (p.Results.Type)
+    case 'mat'
+        theDir = fullfile(theCurrentDir,'matfiles');
+        fileid = fullfile(theDir,[name '.mat']);
+    case 'figure'
+        theDir = fullfile(theCurrentDir,'figures');
+        fileid = fullfile(theDir,[name '.' p.Results.FigureType]);
+
+    case {'movie','movieFile'}
+        theDir = fullfile(theCurrentDir,'movies');
+        fileid = fullfile(theDir,[name '.' p.Results.MovieType]);
+end
+
+%% Make directory if it doesn't exist
+if (~exist(theDir,'dir'))
+    mkdir(theDir);
+end
 
 end
--- toolbox/@IBIOColorDetectReadWriteBasic/write.m
+++ toolbox/@IBIOColorDetectReadWriteBasic/write.m
@@ -14,88 +14,24 @@ function write(obj,name,data,varargin)
 %   'FigureType'
 %      Type of figure to save, as supported by FigureSave/saveas
 %      'pdf' - PDF (default)
-%   'MovieExtension'
+%   'MovieType'
 %      Extension for movie filename
 %      'm4v' - MPEG-4 (default)
 
-%% Parse input.
-p = inputParser;
-p.addRequired('name',@ischar);
-p.addRequired('data');
-p.addParameter('Type','mat',@ischar);
-p.addParameter('ArtifactParams','mat',@isstruct);
-p.addParameter('FigureType','pdf',@ischar);
-p.addParameter('MovieExtension','m4v',@ischar);
-p.parse(name,data,varargin{:});
-
-%% Get parent directory list and make sure the output tree is in place
-theParentDir = '';
-if (~isempty(obj.parentParamsList))
-    for ii = 1:length(obj.parentParamsList)
-        thisParentParams = obj.parentParamsList{ii};
-        switch(obj.parentParams.type)
-            case 'ResponseGeneration'
-                thisParentDir = obj.paramsToResponseGenerationDirName(thisParentParams);
-            case 'ColorModulation'
-                thisParentDir = obj.paramsToColorModulationDirName(thisParentParams);
-            otherwise
-                error('Unkown parent parameters type');
-        end
-        theParentDir = fullfile(theParentDir,thisParentDir);
-        if (~exist(theParentDir,'dir'))
-            mkdir(theParentDir);
-        end
-    end
-    theParentDir = fullfile(getpref('IBIOColorDetect','outputBaseDir'),theParentDir);
-else
-    theParentDir = getpref('IBIOColorDetect','outputBaseDir');
-    if (~exist(theParentDir,'dir'))
-        mkdir(theParentDir);
-    end
-end
-
-%% Get current dir name and make it if necessary
-theCurrentDir = theParentDir;
-for ii = 1:length(obj.currentParamsList)
-    thisCurrentParams = obj.currentParamsList{ii};
-    switch(thisCurrentParams.type)
-        case 'ResponseGeneration'
-            thisCurrentDir = obj.paramsToResponseGenerationDirName(thisCurrentParams);
-        case 'ColorModulation'
-            thisCurrentDir = obj.paramsToColorModulationDirName(thisCurrentParams);
-        otherwise
-            error('Unkown current parameters type');
-    end
-    theCurrentDir = fullfile(theCurrentDir,thisCurrentDir);
-    if (~exist(theCurrentDir,'dir'))
-        mkdir(theCurrentDir);
-    end
-end
-
-%% Compose directory where data should go
-switch (p.Results.Type)
-    case 'mat'
-        theDir = fullfile(theCurrentDir,'matfiles');
-    case 'figure'
-        theDir = fullfile(theCurrentDir,'figures');
-    case {'movie','movieFile'}
-        theDir = fullfile(theCurrentDir,'movies');
-end
-if (~exist(theDir,'dir'))
-    mkdir(theDir);
-end
+%% Get fileid
+fileid = obj.getid(name,varargin{:});
 
 %% Write the data
 switch (p.Results.Type)
     case 'mat'
-        save(fullfile(theDir,name),data,'-v7.3');
+        save(fileid),data,'-v7.3');
     case 'figure'
         if (exist('FigureSave','file'))
-            FigureSave(fullfile(theDir,name),data,p.Results.FigureType);
+            FigureSave(fullfile(fileid,data,p.Results.FigureType);
         else
-            saveas(h,fullfile(theDir,name),p.Results.FigureType);
+            saveas(h,fileid,p.Results.FigureType);
         end
     case 'movieFile'
-        unix(['mv ' data ' ' fullfile(theDir,name) '.' p.Results.MovieExtension]);
+        unix(['mv ' data ' ' fileid]);
 end
 
