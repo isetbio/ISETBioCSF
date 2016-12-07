ddd0a20eec4a93bf9ed36d6b6ff44e16ad06a7ac
Nicolas Cottaris <cottaris@sas.upenn.edu>
2016-12-07 12:32:28 -0500

    Updates
    Signed-off-by: Nicolas Cottaris <cottaris@sas.upenn.edu>

--- toolbox/conemosaic/colorDetectMultiTrialEMPathGenerate.m
+++ toolbox/conemosaic/colorDetectMultiTrialEMPathGenerate.m
@@ -7,17 +7,17 @@ function theEMpaths = colorDetectMultiTrialEMPathGenerate(theConeMosaic, nTrials
 %  11/30/16  npc Wrote it.
 %
 switch (emPathType)
-    case 'Zero'
+    case 'none'
         theEMpaths = zeros(nTrials, eyeMovementsPerTrial, 2);     
-    case 'Frozen'
+    case 'frozen'
         theFixedEMpath = theConeMosaic.emGenSequence(eyeMovementsPerTrial);
         theEMpaths = permute(repmat(theFixedEMpath, [1 1 nTrials]), [3 1 2]);
-    case 'Dynamic'
+    case 'random'
         theEMpaths = zeros(nTrials, eyeMovementsPerTrial, 2);
         for iTrial= 1:nTrials
             theEMpaths(iTrial, :,:) = theConeMosaic.emGenSequence(eyeMovementsPerTrial);
         end
     otherwise
-        error('Unknown emPathType: ''%s''. Valid choices: ''Zero'', ''Frozen'', ''Dynamic''.', emPathType);
+        error('Unknown emPathType: ''%s''. Valid choices: ''none'', ''frozen'', ''random''.', emPathType);
 end
 end
