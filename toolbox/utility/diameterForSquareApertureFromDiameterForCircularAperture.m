774fd4789ee25bb115acba0eee2954ad570a8f79
David Brainard <brainard@psych.upenn.edu>
2016-12-19 07:50:18 -0500

    Change name, add comment.
    Signed-off-by: David Brainard <brainard@psych.upenn.edu>

--- toolbox/utility/diameterForSquareApertureFromDiameterForCircularAperture.m
+++
@@ -1,5 +0,0 @@
-function squareApertureDiameter = diameterForSquareApertureFromDiameterForCircularAperture(circularApertureDiameter)
-    area = pi * (circularApertureDiameter/2)^2;
-    squareApertureDiameter = sqrt(area);
-end
-
