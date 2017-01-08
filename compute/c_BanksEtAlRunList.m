c6e7eaa5cb435b2a217b329c42f0ee00bfa4bd5f
David Brainard <brainard@psych.upenn.edu>
2017-01-08 12:28:37 -0500

    Typo fix.

--- compute/c_BanksEtAlRunList.m
+++ compute/c_BanksEtAlRunList.m
@@ -2,6 +2,9 @@
 %
 % Run the Banks et al. computations with various set parameters.
 
+% Clear and close
+clear; close all;
+
 % Common parameters
 params.useScratchTopLevelDirName = false;
 params.computeResponses = true;
@@ -11,7 +14,7 @@ params.nTrainingSamples = 1000;
 params.conePacking = 'hexReg';
 params.mosaicRotationDegs = 30;
 params.coneSpacingMicrons = 3;
-params.innerSegmentSizeMicrons = sizeForSquareApertureFromDiameterForCircularAperture(params.coneSpacingMicron);
+params.innerSegmentSizeMicrons = sizeForSquareApertureFromDiameterForCircularAperture(params.coneSpacingMicrons);
 params.cyclesPerDegree = [3 5 10 20 40 50 60];
 params.luminances = [3.4 34 340];
 params.pupilDiamMm = 2;
@@ -24,17 +27,17 @@ params.visualizeResponses = false;
 % No blur, no dark noise, no aperture blur
 params.blur = false;
 params.apertureBlur = false;
-params.darkNoise = [0 0 0];
+params.coneDarkNoiseRate = [0 0 0];
 c_BanksEtAlReplicate(params);
 
 % No blur, no dark noise, with aperture blur
 params.blur = false;
 params.apertureBlur = true;
-params.darkNoise = [0 0 0];
+params.coneDarkNoiseRate = [0 0 0];
 c_BanksEtAlReplicate(params);
 
 % With blur, no dark noise, with aperture blur
 params.blur = true;
 params.apertureBlur = true;
-params.darkNoise = [0 0 0];
+params.coneDarkNoiseRate = [0 0 0];
 c_BanksEtAlReplicate(params);
