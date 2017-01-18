5c2badc6d75533e9d12bc67ec1f927c235249fdb
Nicolas Cottaris <cottaris@sas.upenn.edu>
2017-01-18 17:24:22 -0500

    Batch job.
    Signed-off-by: Nicolas Cottaris <cottaris@sas.upenn.edu>

--- toolbox/responseInstanceGenerator/colorDetectResponseInstanceArrayFastConstruct.m
+++ toolbox/responseInstanceGenerator/colorDetectResponseInstanceArrayFastConstruct.m
@@ -169,7 +169,7 @@ function trialBlockSize = computeTrialBlockSizeForParforLoop(nTrials, coneMosaic
     singleTrialMemoryGBytes = numberOfCores * (obj_absorptions_memsize  +  obj_currents_memsize + absorptions_memsize + photocurrents_memsize)/(1024^3);
     
     allowedRAMcompression = 1.0;
-    trialBlockSize = floor(allowedRAMcompression*ramSizeGBytesAvailable/singleTrialMemoryGBytes);
+    trialBlockSize = round(allowedRAMcompression*ramSizeGBytesAvailable/singleTrialMemoryGBytes);
     if (trialBlockSize > nTrials)
         trialBlockSize = nTrials;
     end
