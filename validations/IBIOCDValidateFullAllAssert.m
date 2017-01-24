c217c8022839b620d28b4041d8d92f6935b0e56e
Ben Heasly <benjamin.heasly@gmail.com>
2017-01-24 12:50:05 -0500

    IBIOCDValidateFullAllAssert throw exception when validation fails

---
+++ validations/IBIOCDValidateFullAllAssert.m
@@ -0,0 +1,66 @@
+function IBIOCDValidateFullAllAssert(varargin)
+% IBIOCDValidateFullAllAssert(varargin)
+%
+% Full data check (no figures, no publish) of all validation functions.
+%
+% If any validation fails, throw an exception.
+%
+% Optional key/value pairs
+%   'verbosity' - string (default 'low').  How chatty to be about output.
+%      'none' - Don't say anything.
+%      'low' - Minimal.
+%      'medium' - As the name suggests.
+%      'high' - More than medium.
+%      'max' - As much as possible
+%   'generatePlots' - true/false (default false).  Generate plots?
+%   'graphMismatchedData' - true/false (default true).  Make a graph when
+%       validation fails?
+%   'numericTolerance' - value (default 500*eps).  Tolerance to use for numeric checks.
+
+%% Close all figures so that we start with a clean slate
+close all; 
+
+%% We will use preferences for the 'isetbioValidation' project
+thisProject = 'IBIOColorDetect';
+UnitTest.usePreferencesForProject(thisProject);
+
+%% Parse input and set settable prefs
+p = inputParser;
+p.addParameter('verbosity','low',@ischar);
+p.addParameter('generatePlots',false,@islogical);
+p.addParameter('graphMismatchedData',false,@islogical);
+p.addParameter('numericTolerance',500*eps,@isnumeric);
+p.parse(varargin{:});
+UnitTest.setPref('verbosity',p.Results.verbosity);
+UnitTest.setPref('generatePlots',p.Results.generatePlots);
+UnitTest.setPref('graphMismatchedData',p.Results.graphMismatchedData);
+UnitTest.setPref('numericTolerance',p.Results.numericTolerance);
+
+%% Set other preferences for this function
+
+% Run time error behavior
+% valid options are: 'rethrowExceptionAndAbort', 'catchExceptionAndContinue'
+UnitTest.setPref('onRunTimeErrorBehavior', 'catchExceptionAndContinue');
+
+% Plot generation
+UnitTest.setPref('closeFigsOnInit', true);
+              
+%% Print current values of isetbioValidation prefs
+UnitTest.listPrefs();
+
+%% What to validate
+listingScript = UnitTest.getPref('listingScript');
+vScriptsList = eval(listingScript);
+
+%% How to validate
+%
+% Run a FULL validation session (comparing actual data)
+obj = UnitTest.runValidationSession(vScriptsList, 'FULLONLY');
+
+% assert no failed validations
+summary = [obj.summaryReport{:}];
+success = ~any([summary.fullFailed]);
+assert(success, 'One or more validations failed.');
+
+
+end
