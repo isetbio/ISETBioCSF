596ef92a34c6d1bd027aa3507107c1d38b577c62
David Brainard <brainard@psych.upenn.edu>
2017-02-15 10:30:44 -0500

    More spot sizes
    Signed-off-by: David Brainard <brainard@psych.upenn.edu>

--- compute/c_DavilaGeislerRunList.m
+++ compute/c_DavilaGeislerRunList.m
@@ -6,16 +6,16 @@
 clear; close all;
 
 % Common parameters
-params.computeResponses = false;
-params.findPerformance = false;
-params.fitPsychometric = false;
+params.computeResponses = true;
+params.findPerformance = true;
+params.fitPsychometric = true;
 
 params.useScratchTopLevelDirName = false;
 params.thresholdMethod = 'mlpt';
 
 % Background for production should be 2.1, but can use smaller to make
 % tests run faster.
-params.spotDiametersMinutes = [0.5 1 5 10 20 40];
+params.spotDiametersMinutes = [0.25 0.5 0.75 1 2 5 7.5 10 15 20 40];
 params.backgroundSizeDegs = 1;
 params.luminances = 10;
 
