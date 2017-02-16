46f6faccf276f02f8bb659e7e0e450b99e5ab87f
Nicolas Cottaris <cottaris@sas.upenn.edu>
2017-02-16 16:40:59 -0500

    updates
    Signed-off-by: Nicolas Cottaris <cottaris@sas.upenn.edu>

--- toolbox/responseInstanceGenerator/colorDetectResponseInstanceArrayFastConstruct.m
+++ toolbox/responseInstanceGenerator/colorDetectResponseInstanceArrayFastConstruct.m
@@ -200,18 +200,18 @@ function trialBlockSize = computeTrialBlockSizeForParforLoop(nTrials, coneMosaic
     oi_memsize = 3 * oiSizeBytes;
     
     % mosaic.compute temporary products
-    computeTempProductsSize = coneMosaicPatternSize*emPathLength*sizeOfDoubleInBytes;
+    computeTempProductsSize = coneMosaicPatternSize*emPathLength*sizeOfDoubleInBytes
     
     % Absorptions
-    obj_absorptions_memsize = coneMosaicActivePatternSize*emPathLength*sizeOfDoubleInBytes;
+    obj_absorptions_memsize = coneMosaicActivePatternSize*emPathLength*sizeOfDoubleInBytes
     absorptions_memsize = coneMosaicActivePatternSize*emPathLength*sizeOfDoubleInBytes/2;
     
     % Photocurrents
     obj_currents_memsize = 0*obj_absorptions_memsize;
-    photocurrents_memsize = absorptions_memsize;
+    photocurrents_memsize = absorptions_memsize
     
     % Compute trialBlocksSize
-    singleTrialMemoryGBytes = numberOfCores * (computeTempProductsSize + obj_absorptions_memsize + absorptions_memsize + max([oi_memsize obj_currents_memsize+photocurrents_memsize]))/(1024^3);
+    singleTrialMemoryGBytes = numberOfCores * (max([computeTempProductsSize obj_absorptions_memsize+absorptions_memsize+max([oi_memsize obj_currents_memsize+photocurrents_memsize])]))/(1024^3);
     
     allowedRAMcompression = 1.0;
     trialBlockSize = round(allowedRAMcompression*ramSizeGBytesAvailable/singleTrialMemoryGBytes);
