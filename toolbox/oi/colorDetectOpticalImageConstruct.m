d3c9d42bfe8b97a752c3a8739827dde2c2aab497
Nicolas Cottaris <cottaris@upenn.edu>
2017-08-07 04:43:08 -0400

    Removed plotting of lens density and transmittance
    Plot was added just to double check values for the paper.

--- toolbox/oi/colorDetectOpticalImageConstruct.m
+++ toolbox/oi/colorDetectOpticalImageConstruct.m
@@ -50,16 +50,6 @@ theOI = oiSet(theOI,'h fov',oiParams.fieldOfViewDegs);
 
 % Set the pupil diamter
 focalLength = oiGet(theOI,'distance');
-
-figure(114);
-subplot(1,2,1);
-plot(theOI.optics.lens.wave, theOI.optics.lens.spectralDensity, 'k-');
-subplot(1,2,2);
-plot(theOI.optics.lens.wave, theOI.optics.lens.transmittance, 'k-');
-transmittance555 = 0.5*(theOI.optics.lens.transmittance(find(theOI.optics.lens.wave == 550))+...
-theOI.optics.lens.transmittance(find(theOI.optics.lens.wave == 560)))
-pause
-
 desiredFNumber = focalLength/(oiParams.pupilDiamMm/1000);
 theOI  = oiSet(theOI ,'optics fnumber',desiredFNumber);
 pupilDiamMmCheck = 1000*oiGet(theOI,'optics aperture diameter');
