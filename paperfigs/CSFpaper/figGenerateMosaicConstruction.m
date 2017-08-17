c9c647981742b007b045d66a991f7cd8002b9a55
Nicolas Cottaris <cottaris@upenn.edu>
2017-08-16 20:17:23 -0400

    Updates in figGenerateMosaicConstruction

--- paperfigs/CSFpaper/figGenerateMosaicConstruction.m
+++ paperfigs/CSFpaper/figGenerateMosaicConstruction.m
@@ -6,38 +6,46 @@ ieInit; clear; close all;
 % Set random seed to obtain replicable results
 rng(1235);
 
-% Set coneMosaicHex - specific params
-params.resamplingFactor = 9;                            % how fine to sample the hex mosaic positions with an underlying rect mosaic
-params.fovDegs = 0.25;                                  % FOV in degrees (default: 0.25)
-params.eccBasedConeDensity = true;                      % if true, generate a mosaic where cone spacing varies with eccentricity (Curcio model)
-params.customLambda = [];                               % cone spacing in microns (only used with regular hex mosaics)
-params.rotationDegs = 0;                                % rotation of the mosaic, in degrees 0 = , 30 = (makes sense with regular hex mosaics)
-params.customInnerSegmentDiameter = [];                 % inner segment diameter, in microns (empty for isetbio default) 
-params.sConeMinDistanceFactor = 3.0;                    % min distance between neighboring S-cones = f * local cone separation - to make the S-cone lattice semi-regular
-params.sConeFreeRadiusMicrons = 45;                     % radius of S-cone free retina, in microns
-params.latticeAdjustmentPositionalToleranceF = 0.01/2;  % determines cone delta movement tolerance for terminating iterative adjustment - by default this is 0.01 (here setting it lower for faster, but less acurate mosaic generation)
-params.latticeAdjustmentDelaunayToleranceF = 0.001/2;   % determines position tolerance for triggering another Delaunay triangularization - by default this is 0.001 (here setting it lower for faster, but less acurate mosaic generation)
-saveLatticeAdjustmentProgression = true;                % set to true, only if interested to see how the mosaic lattice is iteratively adjusted when eccBasedConeDensity is true               
+makeNew = false;
+if (makeNew)
+    % Set coneMosaicHex - specific params
+    params.resamplingFactor = 9;                            % 9 is good;how fine to sample the hex mosaic positions with an underlying rect mosaic
+    params.fovDegs = [2.5 1.25]/2;                          % FOV in degrees ([width height], default: 0.25x0.25
+    params.eccBasedConeDensity = true;                      % if true, generate a mosaic where cone spacing varies with eccentricity (Curcio model)
+    params.customLambda = [];                               % cone spacing in microns (only used with regular hex mosaics)
+    params.rotationDegs = 0;                                % rotation of the mosaic, in degrees 0 = , 30 = (makes sense with regular hex mosaics)
+    params.customInnerSegmentDiameter = [];                 % inner segment diameter, in microns (empty for isetbio default) 
+    params.sConeMinDistanceFactor = 3.0;                    % min distance between neighboring S-cones = f * local cone separation - to make the S-cone lattice semi-regular
+    params.sConeFreeRadiusMicrons = 45;                     % radius of S-cone free retina, in microns
+    params.latticeAdjustmentPositionalToleranceF = 0.01*2;  % determines cone delta movement tolerance for terminating iterative adjustment - by default this is 0.01 (here setting it lower for faster, but less acurate mosaic generation)
+    params.latticeAdjustmentDelaunayToleranceF = 0.001*2;   % determines position tolerance for triggering another Delaunay triangularization - by default this is 0.001 (here setting it lower for faster, but less acurate mosaic generation)
+    saveLatticeAdjustmentProgression = true;                % set to true, only if interested to see how the mosaic lattice is iteratively adjusted when eccBasedConeDensity is true               
 
-% Generate the mosaic
-theHexMosaic = coneMosaicHex(params.resamplingFactor, ...
-    'fovDegs', params.fovDegs, ...
-    'eccBasedConeDensity', params.eccBasedConeDensity, ...
-    'rotationDegs', params.rotationDegs, ...
-    'sConeMinDistanceFactor', params.sConeMinDistanceFactor, ...    
-    'sConeFreeRadiusMicrons', params.sConeFreeRadiusMicrons, ...    
-    'customLambda', params.customLambda, ...
-    'customInnerSegmentDiameter', params.customInnerSegmentDiameter, ...
-    'latticeAdjustmentPositionalToleranceF', params.latticeAdjustmentPositionalToleranceF, ...              
-    'latticeAdjustmentDelaunayToleranceF', params.latticeAdjustmentDelaunayToleranceF, ...               
-    'saveLatticeAdjustmentProgression', saveLatticeAdjustmentProgression ...  
-);
+    % Generate the mosaic
+    theHexMosaic = coneMosaicHex(params.resamplingFactor, ...
+        'fovDegs', params.fovDegs, ...
+        'eccBasedConeDensity', params.eccBasedConeDensity, ...
+        'rotationDegs', params.rotationDegs, ...
+        'sConeMinDistanceFactor', params.sConeMinDistanceFactor, ...    
+        'sConeFreeRadiusMicrons', params.sConeFreeRadiusMicrons, ...    
+        'customLambda', params.customLambda, ...
+        'customInnerSegmentDiameter', params.customInnerSegmentDiameter, ...
+        'latticeAdjustmentPositionalToleranceF', params.latticeAdjustmentPositionalToleranceF, ...              
+        'latticeAdjustmentDelaunayToleranceF', params.latticeAdjustmentDelaunayToleranceF, ...               
+        'saveLatticeAdjustmentProgression', saveLatticeAdjustmentProgression ...  
+    );
+    % Save the mosaic
+    save('theHexMosaic.mat', 'theHexMosaic', '-v7.3');
+else
+    % Load the mosaic
+    load('theHexMosaic.mat');
+end
 
-% Display mosaic info
+% Display mosaic info(
 theHexMosaic.displayInfo();
 
 % Show how the lattice of an ecc-based cone density hex mosaic is iteratively adjusted
-%theHexMosaic.plotMosaicProgression();
+theHexMosaic.plotMosaicProgression();
 
 % Show the final LMS mosaic
 visualizedAperture = 'both'; % choose between 'both', 'lightCollectingArea', 'geometricArea'
