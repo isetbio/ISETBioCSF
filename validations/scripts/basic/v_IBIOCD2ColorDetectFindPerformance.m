0c357d1d135c01d06995ea7d6fa62927996046ac
Nicolas Cottaris <cottaris@upenn.edu>
2017-08-18 17:42:04 -0400

    Updated rParams to match those of v_IBIOCD1ConeCurrentEyeMovementsResponseInstances

--- validations/scripts/basic/v_IBIOCD2ColorDetectFindPerformance.m
+++ validations/scripts/basic/v_IBIOCD2ColorDetectFindPerformance.m
@@ -16,8 +16,30 @@ function ValidationFunction(runTimeParams)
     %% Hello
     UnitTest.validationRecord('SIMPLE_MESSAGE', '***** v_IBIOCD2ColorDetectFindPerformance *****');
     
+        %% Get run params
+    rParams = responseParamsGenerate;
+    
+    % Override some defult parameters
+    rParams.spatialParams.fieldOfViewDegs = 1.0;
+    rParams.spatialParams.cyclesPerDegree = 8;
+    rParams.spatialParams.gaussianFWHMDegs = 0.375;
+    % Set duration equal to sampling interval to do just one frame.
+    rParams.temporalParams.stimulusDurationInSeconds = 200/1000;
+    rParams.temporalParams.stimulusSamplingIntervalInSeconds = rParams.temporalParams.stimulusDurationInSeconds;
+    rParams.temporalParams.secondsToInclude = rParams.temporalParams.stimulusDurationInSeconds;
+    % Mosaic params
+    rParams.mosaicParams.integrationTimeInSeconds = rParams.temporalParams.stimulusDurationInSeconds;
+    rParams.mosaicParams.isomerizationNoise = 'frozen';         % Type coneMosaic.validNoiseFlags to get valid values
+    rParams.mosaicParams.osNoise = 'frozen';                    % Type outerSegment.validNoiseFlags to get valid values
+    rParams.mosaicParams.osModel = 'Linear';
+    rParams.mosaicParams.fieldOfViewDegs = 1.0;
+    
+    testDirectionParams = instanceParamsGenerate;
+    testDirectionParams.trialsNum = 64;
+    
     %% Basic validation
     [validationData1,extraData1] = t_colorDetectFindPerformance(...
+        'rParams', rParams, ...
         'employStandardHostComputerResources', true,...
         'generatePlots', runTimeParams.generatePlots,...
         'plotPsychometric', true,...
