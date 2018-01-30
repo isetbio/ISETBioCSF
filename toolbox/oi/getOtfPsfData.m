5f2963f3ae8f63d2b62e6c357c0814606c39ae94
Nicolas Cottaris <cottaris@sas.upenn.edu>
2018-01-30 15:16:51 -0500

    Updated getOtfPsfData
    Signed-off-by: Nicolas Cottaris <cottaris@sas.upenn.edu>

--- toolbox/oi/getOtfPsfData.m
+++ toolbox/oi/getOtfPsfData.m
@@ -1,13 +1,22 @@
-function [otf, otf_fx, otf_fy, psf, psf_x, psf_y] = getOtfPsfData(theOI, selectedWavelength)
+% Method to get the OTF and the PSF for a particular wavelength (as well as the microns/degree factor) from an oi
+function [otf, otf_fxCyclesPerDeg, otf_fyCyclesPerDeg, psf, psf_xMinutes, psf_yMinutes, micronsPerDegree] = getOtfPsfData(theOI, selectedWavelength)
+    % Get OTF
     theOptics = oiGet(theOI, 'optics');
     otf = abs(fftshift(opticsGet(theOptics,'otf data', selectedWavelength)));
-    otf_fx = opticsGet(theOptics, 'otf fx', 'cyclesperdeg');
-    otf_fy = opticsGet(theOptics, 'otf fx', 'cyclesperdeg');
+    otf_fxCyclesPerDeg = opticsGet(theOptics, 'otf fx', 'cyclesperdeg');
+    otf_fyCyclesPerDeg = opticsGet(theOptics, 'otf fx', 'cyclesperdeg');
+    [xSfGridCyclesDegGrid,ySfGridCyclesDegGrid] = meshgrid(otf_fxCyclesPerDeg, otf_fyCyclesPerDeg);
     
-    psf = opticsGet(theOptics,'psf data', selectedWavelength);
-    %psfSupport = opticsGet(theOptics,'psf support', 'deg');
-    [X,Y] = meshgrid(otf_fx,otf_fy);
-    [xGridMinutes,yGridMinutes] = SfGridCyclesDegToPositionGridMinutes(X,Y);
-    psf_x = xGridMinutes(1,:);
-    psf_y = psf_x;
+    % Get PSF and its support (in minutes) via OtfToPsf
+    [xGridMinutes,yGridMinutes,psf] = OtfToPsf(xSfGridCyclesDegGrid,ySfGridCyclesDegGrid,otf);
+    psf_xMinutes = squeeze(xGridMinutes(1,:));
+    psf_yMinutes = squeeze(yGridMinutes(:,1));
+            
+    % Get psf support in microns
+    psfMicrons = opticsGet(theOptics,'psf support','um');
+    psfXsupportMicrons = psfMicrons{1}(1,:);
+
+    % Compute microns-per-degree for this OI
+    psfXsupportDegrees = psf_xMinutes*60;
+    micronsPerDegree = psfXsupportMicrons(1)/psfXsupportDegrees(1);
 end
