f5d3f03b7d9c0cb493439f47c4b5119c9a79a928
Nicolas Cottaris <cottaris@sas.upenn.edu>
2018-03-14 14:56:15 -0400

    Updates
    Signed-off-by: Nicolas Cottaris <cottaris@sas.upenn.edu>

--- paperfigs/CSFpaper/generateFigureForPaper.m
+++ paperfigs/CSFpaper/generateFigureForPaper.m
@@ -2,9 +2,14 @@ function generateFigureForPaper(theFigData, variedParamLegends, variedParamName,
 
     p = inputParser;
     p.addParameter('figureType', 'CSF', @ischar);
+    p.addParameter('showBanksPaperIOAcurves', false, @islogical);
+    p.addParameter('plotFirstConditionInGray', true, @islogical);
+    
     p.parse(varargin{:});
     
     figureType = p.Results.figureType;
+    showBanksPaperIOAcurves = p.Results.showBanksPaperIOAcurves;
+    plotFirstConditionInGray = p.Results.plotFirstConditionInGray;
     
     for condIndex = 1:numel(theFigData)
         figData = theFigData{condIndex};
@@ -21,18 +26,37 @@ function generateFigureForPaper(theFigData, variedParamLegends, variedParamName,
     
     
     % Displayed colors
-    colors(1,:) = [0.5 0.5 0.5];
-    if (numel(theFigData)>1)
-        colors(2:numel(theFigData),:) = brewermap(numel(theFigData)-1, 'Set1');
+    if (plotFirstConditionInGray)
+        colors(1,:) = [0.5 0.5 0.5];
+        if (numel(theFigData)>1)
+            colors(2:numel(theFigData),:) = brewermap(numel(theFigData)-1, 'Set1');
+        end
+    else
+        colors = brewermap(numel(theFigData), 'Set1');
     end
+    
     faceColor = [0.3 0.3 0.3];
     
     hold on;
-    for condIndex = 1:numel(theFigData)
-        plot(cpd{condIndex}, contrastSensitivity{condIndex}, 'o-', 'Color', squeeze(colors(condIndex,:)), ...
-        'MarkerEdgeColor', squeeze(colors(condIndex,:)), ...
-        'MarkerFaceColor', min(1, squeeze(colors(condIndex,:)) + faceColor), ...
-        'MarkerSize',12,'LineWidth',2);
+
+    if (showBanksPaperIOAcurves)
+        banksFactor = 1;
+        plot(figData.C(:,1),figData.C(:,2)*banksFactor,'-','Color', squeeze(colors(3,:)), 'LineWidth',2);
+        plot(figData.D(:,1),figData.D(:,2)*banksFactor,'-','Color', squeeze(colors(2,:)), 'LineWidth',2);
+        plot(figData.E(:,1),figData.E(:,2)*banksFactor,'-','Color', squeeze(colors(1,:)), 'LineWidth',2);
+        for condIndex = 1:numel(theFigData)
+            plot(cpd{condIndex}, contrastSensitivity{condIndex}, 'o', 'Color', squeeze(colors(condIndex,:)), ...
+            'MarkerEdgeColor', squeeze(colors(condIndex,:)), ...
+            'MarkerFaceColor', min(1, squeeze(colors(condIndex,:)) + faceColor), ...
+            'MarkerSize',12,'LineWidth',2);
+        end
+    else
+        for condIndex = 1:numel(theFigData)
+            plot(cpd{condIndex}, contrastSensitivity{condIndex}, 'o-', 'Color', squeeze(colors(condIndex,:)), ...
+            'MarkerEdgeColor', squeeze(colors(condIndex,:)), ...
+            'MarkerFaceColor', min(1, squeeze(colors(condIndex,:)) + faceColor), ...
+            'MarkerSize',12,'LineWidth',2);
+        end
     end
     
     % Add legend
