45e6926748802a00c61571db1da37236ffdd1c3e
Nicolas Cottaris <cottaris@sas.upenn.edu>
2018-03-23 14:07:24 -0400

    Updated eye movement analysis plot code
    Signed-off-by: Nicolas Cottaris <cottaris@sas.upenn.edu>

--- paperscripts/CSFpaper/run_EyeMovementVaryConditions.m
+++ paperscripts/CSFpaper/run_EyeMovementVaryConditions.m
@@ -3,10 +3,10 @@ function run_EyeMovementVaryConditions
 %  
     % How to split the computation
     % 0 (All mosaics), 1; (Largest mosaic), 2 (Second largest), 3 (all 2 largest)
-    computationInstance = 1;
+    computationInstance = 0;
     
     % Whether to make a summary figure with CSF from all examined conditions
-    makeSummaryFigure = ~true;
+    makeSummaryFigure = true;
     
     % Mosaic to use
     mosaicName = 'ISETbioHexEccBasedLMSrealistic'; 
@@ -36,32 +36,68 @@ function run_EyeMovementVaryConditions
     examinedCond(1).classifier = 'svmV1FilterBank';
     examinedCond(1).legend = 'no eye movements, SVM (QPhE)';
     examinedCond(1).centeredEMpaths = true;
+    examinedCond(1).frameRate = 20;
+    examinedCond(1).responseStabilizationMilliseconds = 100;
+    examinedCond(1).responseExtinctionMilliseconds = 50;
+    
+%     examinedCond(6).emPathType = 'frozen0';
+%     examinedCond(6).classifier = 'svmV1FilterBank';
+%     examinedCond(6).legend = 'no eye movements(2), SVM (QPhE)';
+%     examinedCond(6).centeredEMpaths = true;
+%     examinedCond(6).frameRate = 10;
+%     examinedCond(6).responseStabilizationMilliseconds = 40;
+%     examinedCond(6).responseExtinctionMilliseconds = 40;
+    
     
     examinedCond(2).emPathType = 'random';
     examinedCond(2).classifier = 'svm';
-    examinedCond(2).legend = 'drifts+\mu-saccades, SVM';
-    examinedCond(2).centeredEMpaths = true;
+    examinedCond(2).legend = 'drifts+\mu-saccades (origin), SVM';
+    examinedCond(2).centeredEMpaths = ~true;
+    examinedCond(2).frameRate = 10;
+    examinedCond(2).responseStabilizationMilliseconds = 40;
+    examinedCond(2).responseExtinctionMilliseconds = 40;
     
     examinedCond(3).emPathType = 'random';
     examinedCond(3).classifier = 'svmV1FilterBank';
-    examinedCond(3).legend = 'drifts+\mu-saccades, SVM (QPhE)';
-    examinedCond(3).centeredEMpaths = true;
-
+    examinedCond(3).legend = 'drifts+\mu-saccades (origin), SVM (QPhE)';
+    examinedCond(3).centeredEMpaths = ~true;
+    examinedCond(3).frameRate = 10;
+    examinedCond(3).responseStabilizationMilliseconds = 40;
+    examinedCond(3).responseExtinctionMilliseconds = 40;
+    
+    examinedCond(4).emPathType = 'random';
+    examinedCond(4).classifier = 'svm';
+    examinedCond(4).legend = 'drifts+\mu-saccades (random), SVM';
+    examinedCond(4).centeredEMpaths = true;
+    examinedCond(4).frameRate = 20;
+    examinedCond(4).responseStabilizationMilliseconds = 100;
+    examinedCond(4).responseExtinctionMilliseconds = 50;
+    
+    examinedCond(5).emPathType = 'random';
+    examinedCond(5).classifier = 'svmV1FilterBank';
+    examinedCond(5).legend = 'drifts+\mu-saccades (random), SVM (QPhE)';
+    examinedCond(5).centeredEMpaths = true;
+    examinedCond(5).frameRate = 20;
+    examinedCond(5).responseStabilizationMilliseconds = 100;
+    examinedCond(5).responseExtinctionMilliseconds = 50;
+    
+    
     % Simulation steps to perform
     params.computeMosaic = ~true; 
     params.visualizeMosaic = ~true;
     
-    params.computeResponses = true;
+    params.computeResponses = ~true;
     params.computePhotocurrentResponseInstances = ~true;
     params.visualizeResponses = ~true;
     params.visualizeSpatialScheme = ~true;
+    params.visualizeSpatialPoolingScheme = ~true;
     params.visualizeOIsequence = ~true;
     params.visualizeOptics = ~true;
     params.visualizeMosaicWithFirstEMpath = ~true;
     
     params.visualizeKernelTransformedSignals = ~true;
     params.findPerformance = ~true;
-    params.visualizePerformance = ~true;
+    params.visualizePerformance = true;
     params.deleteResponseInstances = ~true;
     
     % When computing responses only do the first  conditions 1 and 3
@@ -76,6 +112,9 @@ function run_EyeMovementVaryConditions
         cond = examinedCond(condIndex);
         params.emPathType = cond.emPathType;
         params.centeredEMPaths = cond.centeredEMpaths;
+        params.frameRate = cond.frameRate;
+        params.responseStabilizationMilliseconds = cond.responseStabilizationMilliseconds;
+        params.responseExtinctionMilliseconds = cond.responseExtinctionMilliseconds;
         params.performanceClassifier = cond.classifier;
         examinedEyeMovementTypeLegends{condIndex} = cond.legend;
         [~,~, theFigData{condIndex}] = run_BanksPhotocurrentEyeMovementConditions(params);
