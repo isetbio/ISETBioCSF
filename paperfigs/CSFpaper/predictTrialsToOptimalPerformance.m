e2c7fa8980f96f2c1a6f8a3a0a51d5e9fe4827ad
Nicolas Cottaris <cottaris@sas.upenn.edu>
2018-06-07 14:17:57 -0400

    Updates to predictTrialsToOptimalPerformance

--- paperfigs/CSFpaper/predictTrialsToOptimalPerformance.m
+++ paperfigs/CSFpaper/predictTrialsToOptimalPerformance.m
@@ -9,15 +9,16 @@ function predictTrialsToOptimalPerformance
     load('/Users/nicolas/Desktop/theData32.mat', 'theData')
     [trialsLog32, mlpt32, svmPCA32, svmPool32] = analyzeData(theData);
     
-    figure(1); clf;
-    subplot(1,3,1); 
-    plotData(trialsLog8, mlpt8, svmPCA8, svmPool8);
+    hFig = figure(1); clf;
+    set(hFig, 'Color', [1 1 1], 'Position', [10 10 1000 1000]);
+    subplot(2,2,1); 
+    plotData(trialsLog8, mlpt8, svmPCA8, svmPool8, '8 c/deg');
     
-    subplot(1,3,2); 
-    plotData(trialsLog16, mlpt16, svmPCA16, svmPool16);
+    subplot(2,2,2); 
+    plotData(trialsLog16, mlpt16, svmPCA16, svmPool16, '16 c/deg');
     
-    subplot(1,3,3); 
-    plotData(trialsLog32, mlpt32, svmPCA32, svmPool32);
+    subplot(2,2,3); 
+    plotData(trialsLog32, mlpt32, svmPCA32, svmPool32, '32 c/deg');
     
 end
 
@@ -40,12 +41,19 @@ function [trialsLog, mlpt, svmPCA, svmPool] = analyzeData(theData)
     
 end
 
-function plotData(trialsLog, mlpt, svmPCA, svmPool)
+function plotData(trialsLog, mlpt, svmPCA, svmPool, titleText)
     
-    plot([2 30], mlpt.csLog(1)*[1 1], 'k--'); hold on;
-    plot(trialsLog, svmPCA.csLog, 'ks-'); hold on;
-    plot(trialsLog, svmPool.csLog, 'rs-'); hold on;
-    plot(svmPCA.csLogExtrapX, svmPCA.csLogExtrapY, 'b.-');
-    plot(svmPool.csLogExtrapX, svmPool.csLogExtrapY, 'r.-');
-    set(gca, 'XLim', [2 20], 'YLim', [0.1 3]);
+    plot([2 30], mlpt.csLog(1)*[1 1], 'k--', 'MarkerSize', 12); hold on;
+    plot(trialsLog, svmPCA.csLog, 'ks-', 'MarkerSize', 12, 'MarkerFaceColor', [0.5 0.5 0.50]); hold on;
+    plot(trialsLog, svmPool.csLog, 'rs-', 'MarkerSize', 12, 'MarkerFaceColor', [0.9 0.5 0.50]); hold on;
+    plot(svmPCA.csLogExtrapX, svmPCA.csLogExtrapY, 'k-', 'LineWidth', 1.5);
+    plot(svmPool.csLogExtrapX, svmPool.csLogExtrapY, 'r-', 'LineWidth', 1.5);
+    hL = legend('MLPT', 'SVM-PCA', 'SVM-Pool', 'SVM-PCA fit', 'SVM-Pool fit');
+    set(hL, 'Location', 'SouthEast', 'FontSize', 14)
+    set(gca, 'XLim', [2 8], 'YLim', [0.1 3], 'FontSize', 16);
+    grid on; box on
+    xlabel('log10 trials', 'FontWeight', 'bold');
+    ylabel('log10 CSF', 'FontWeight', 'bold');
+    axis 'square'
+    title(titleText);
 end
