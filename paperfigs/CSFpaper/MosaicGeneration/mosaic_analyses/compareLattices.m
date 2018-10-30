83f10303fa9f3bc5ef023f9da16623e877bc07e9
Nicolas Cottaris <cottaris@sas.upenn.edu>
2018-10-29 23:19:05 -0400

    Updates

--- paperfigs/CSFpaper/MosaicGeneration/mosaic_analyses/compareLattices.m
+++ paperfigs/CSFpaper/MosaicGeneration/mosaic_analyses/compareLattices.m
@@ -6,8 +6,7 @@ function compareLattices
     qDistISETBio = computeQuality(coneLocsDegsISETBio);
     qDistBradley = computeQuality(coneLocsDegsBradley);
     
-    
-    figure(1);
+    figure(1); clf;
     subplot(2,2,1)
     plotCones(coneLocsDegsISETBio, maxEcc, 'ISETBio');
     
@@ -19,8 +18,68 @@ function compareLattices
 
     subplot(2,2,4);
     plotQuality(qDistBradley, 'Bradley');
-    
 end
+
+function plotFourier(coneLocs, xo, yo, deltaDegs)
+
+    figure(2); clf;
+    
+    % Select subset of cones within deltaDegs * 1.5 radius of (xo,yo)
+    x = coneLocs(:,1);
+    y = coneLocs(:,2);
+    
+    idx = find(sqrt((x-xo).^2 + (y-yo).^2)<= deltaDegs);
+    coneLocsSubset = coneLocs(idx,:);
+    fprintf('Analyzed cones: %d\n', size(coneLocsSubset,1));
+    
+    % zero-center the positions of this subset of cones
+    coneLocsSubset = coneLocsSubset - mean(coneLocsSubset,1);
+
+    % Generate an image of these cones by digitizing their positions in a
+    % grid of 2*N x 2*N pixels
+    N = 1024;
+    maxLoc = max(abs(coneLocsSubset(:)));
+    deltaLoc = maxLoc/(N-1);
+    coneLocsSubset = round(coneLocsSubset/deltaLoc);
+    N2 = 4096*2;
+    mosaicImage = zeros(N2,N2);
+    for k = 1:size(coneLocsSubset,1)
+        mosaicImage(N2/2+coneLocsSubset(k,1), N2/2+coneLocsSubset(k,2))= 1.0;
+    end
+    
+    % Generate Gaussian envelope
+    xn = -(N2/2):((N2/2)-1);
+    yn = xn;
+    [X,Y] = meshgrid(xn,yn);
+    gaussianEnvelopeSigma = N2/6;
+    gaussianEnvelope = exp(-0.5*(X/gaussianEnvelopeSigma).^2) .* exp(-0.5*(Y/gaussianEnvelopeSigma).^2);
+    
+%     figure(20);
+%     subplot(1,3,1);
+%     imagesc(mosaicImage)
+%     axis 'square'
+%     subplot(1,3,2)
+%     imagesc(gaussianEnvelope)
+%     axis 'image'
+%     subplot(1,3,3)
+%     imagesc(mosaicImage .* gaussianEnvelope)
+%     axis 'image'
+%     colormap(gray(1024))
+%     drawnow
+   
+    % Multiply mosaic image with Gaussian envelope image
+    mosaicImage = mosaicImage .* gaussianEnvelope;
+    imagesc(abs(fftshift(fft2(mosaicImage))));
+    axis 'image'
+    NN = 64;
+    set(gca, 'XLim', N2/2+NN*[-1 1], 'YLim', N2/2+NN*[-1 1])
+    colormap(gray(1024));
+
+    set(gca, 'FontSize', 14, 'XTick', [], 'YTick', []);
+
+end
+
+
 
 function plotQuality(qDist, mosaicName)
     qLims = [0.5 1.005]; qBins = [0.3:0.01:1.0];
