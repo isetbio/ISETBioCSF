f4cbb76056ec53aae56822143324fdb79ab6457a
Nicolas Cottaris <cottaris@sas.upenn.edu>
2019-05-12 16:44:05 -0400

    Updates

--- paperscripts/CSFpaper2/fixationalEMDemoFig/makeFixationalEyeMovementFigure.m
+++ paperscripts/CSFpaper2/fixationalEMDemoFig/makeFixationalEyeMovementFigure.m
@@ -3,7 +3,7 @@ function makeFixationalEyeMovementFigure()
     % Generate 512 3-second emPaths with a sample time of 1 msec
     emDurationSeconds = 3;
     sampleTimeSeconds = 1 / 1000;
-    nTrials = 24;
+    nTrials = 1024;
     
     microSaccadeType = 'none'; % , 'heatmap/fixation based', 'none'
     fixEMobj = fixationalEM();
@@ -17,11 +17,11 @@ function makeFixationalEyeMovementFigure()
         
     maxDurationSecondsForFixationSpan = 0.15;
     emPosRange = 5;
-    plotSingleTrialTrajectoryAndFixationMap(fixEMobj, maxDurationSecondsForFixationSpan, emPosRange);
+    plotData(fixEMobj, maxDurationSecondsForFixationSpan, emPosRange);
 end
 
 
-function plotSingleTrialTrajectoryAndFixationMap(fixEMobj, maxDurationSecondsForFixationSpan, emPosRange )
+function plotData(fixEMobj, maxDurationSecondsForFixationSpan, emPosRange )
 
     % Compute fixationMap
     emPosDelta = 0.6;
@@ -32,12 +32,14 @@ function plotSingleTrialTrajectoryAndFixationMap(fixEMobj, maxDurationSecondsFor
         'maxDurationSeconds', maxDurationSecondsForFixationSpan);
     
     % Compute dispacements
+    displacementMode = 'D2';
     nTrials = size(fixEMobj.emPosArcMin,1);
     for trialNo = 1:nTrials
         xPosArcMin = squeeze(fixEMobj.emPosArcMin(trialNo,:,1));
+        yPosArcMin = squeeze(fixEMobj.emPosArcMin(trialNo,:,2));
         [meanD(trialNo,:), ~, meanDscrambled(trialNo,:), ...
              DrandomWalk, timeLagsMilliseconds] = ...
-        fixEMobj.performDisplacementAnalysis(xPosArcMin, fixEMobj.timeAxis, 'mode', 'D1');
+        fixEMobj.performDisplacementAnalysis(yPosArcMin, fixEMobj.timeAxis, 'mode', displacementMode);
     end
     meanD = mean(meanD,1);
     meanDscrambled = mean(meanDscrambled,1);
@@ -53,7 +55,7 @@ function plotSingleTrialTrajectoryAndFixationMap(fixEMobj, maxDurationSecondsFor
     
     % The velocities
     ax = subplot('Position', [0.065 0.1 0.60 0.39]);
-    plotVelocityPlot(ax, fixEMobj, maxDurationSecondsForFixationSpan)
+    plotVelocityDistributionPlot(ax, fixEMobj, maxDurationSecondsForFixationSpan)
     
     % The fixation map
     ax = subplot('Position', [0.725 0.6 0.28 0.39]);
@@ -64,51 +66,61 @@ function plotSingleTrialTrajectoryAndFixationMap(fixEMobj, maxDurationSecondsFor
     
     % The displacement dynamics
     ax = subplot('Position', [0.725 0.1 0.28 0.39]);
-    plotDisplacementPlot(ax,timeLagsMilliseconds, meanD, meanDscrambled);
+    plotDisplacementPlot(ax,timeLagsMilliseconds, meanD, meanDscrambled, displacementMode);
     
     NicePlot.exportFigToPDF('FixationalEMmodelDemo.pdf', hFig, 300)
 end
 
-function plotVelocityPlot(ax, fixEMobj, maxDurationSecondsForFixationSpan)
-    plot(fixEMobj.timeAxis*1000, fixEMobj.velocityArcMin(:, :), ...
-        '-', 'LineWidth', 1.5, 'Color', [0.4 0.4 0.4 0.1]);
-    hold on;
-    plot(fixEMobj.timeAxis*1000, squeeze(mean(fixEMobj.velocityArcMin, 1)), ...
-        'k-', 'Color', [0 0 0], 'LineWidth', 1.5);
+function plotVelocityDistributionPlot(ax, fixEMobj, maxDurationSecondsForFixationSpan)
+
     subjectDR.mean = 30;
     subjectDR.std = 17;
-    plot([fixEMobj.timeAxis(1) fixEMobj.timeAxis(end)]*1000, ...
-        (subjectDR.mean - subjectDR.std) * [1 1], '--', 'Color', [0 .6 .9], 'LineWidth', 2.0);
     subjectDG.mean = 89;
     subjectDG.std = 63;
-    plot([fixEMobj.timeAxis(1) fixEMobj.timeAxis(end)]*1000, ...
-        (subjectDG.mean + subjectDG.std) * [1 1], '--', 'Color', [0 .6 .9], 'LineWidth', 2.0);
-    timeTicks = 0:20:300;
-    set(gca, 'XLim', [0 maxDurationSecondsForFixationSpan*1000], ...
+    
+    allInstancesVelocities = fixEMobj.velocityArcMin(:);
+    [NN, edges] = histcounts(allInstancesVelocities, [0:10:250]);
+    bar(edges(1:(end-1)),NN, 1, 'FaceAlpha', 0.4, 'FaceColor', [0.8 0.8 0.8]);
+    hold on;
+    plot(subjectDR.mean*[1 1], [0 max(NN)], 'k-', 'LineWidth', 1.5, 'Color', [1 0.3 0.4]);
+    plot((subjectDR.mean-subjectDR.std)*[1 1], [0 max(NN)], 'k--', 'LineWidth', 1.5, 'Color', [1 0.3 0.4]);
+    plot(subjectDG.mean*[1 1], [0 max(NN)], 'k-','LineWidth', 1.5, 'Color', [0.2 0.4 1]);
+    plot((subjectDG.mean+subjectDG.std)*[1 1], [0 max(NN)], 'k--','LineWidth', 1.5, 'Color', [0.2 0.4 1]);
+    
+    timeTicks = 0:50:250;
+    set(gca, 'XLim', [0 250], ...
         'XTick', timeTicks, ...
         'XTickLabel', sprintf('%2.0f\n', timeTicks), ...
-        'YLim', [0 300], ...
-        'YTick', [0:50:500], ...
+        'YTick', [], ...
         'FontSize', 16);
-    xlabel('\it time (seconds)');
-    ylabel('\it velocity (arc min / sec)');
+    xlabel('\it velocity (arc min / sec)');
     box on; grid on
 end
 
-function plotDisplacementPlot(ax,timeLagsMilliseconds, meanD, meanDscrambled)
+function plotDisplacementPlot(ax,timeLagsMilliseconds, meanD, meanDscrambled, displacementMode)
     plot(timeLagsMilliseconds, meanD, 'k-', 'LineWidth', 1.5); hold on;
-    plot(timeLagsMilliseconds, meanDscrambled, 'k--', 'LineWidth', 1.5); hold off
+    plot(timeLagsMilliseconds, meanDscrambled, 'k-.', 'LineWidth', 1.5); hold off
     axis 'square';
     box on;
-    yTicks = [0.2 0.5 1 2];
-    yTickLabels = sprintf('%2.2f\n', yTicks);
+    if (strcmp(displacementMode, 'D1'))
+        yTicks = [0.2 0.5 1 2];
+        yLims = [0 2];
+    else
+        yTicks = [0.1 0.3 1 3 10 30];
+        yLims = [0.06  30];
+    end
+    yTickLabels = sprintf('%2.1f\n', yTicks);
     set(gca, 'XScale', 'log', 'Yscale', 'log');
-    set(gca, 'XLim', [1 300], 'XTick', [1 3 10 30 100 300], ...
+    set(gca, 'XLim', [1.7 800], 'XTick', [1 3 10 30 100 300 1000], 'YLim', yLims, ...
         'YTick', yTicks, 'YTickLabel', yTickLabels, ...
         'XScale', 'log', 'YScale', 'log', 'FontSize', 16);
     grid on; box on
-    xlabel('\it time interval (ms)'); 
-    ylabel('\it displacement (arc min)');
+    xlabel('\it time lag (ms)'); 
+    if (strcmp(displacementMode, 'D1'))
+        ylabel('\it displacement (arc min)');
+    else
+        ylabel('\it displacement (arc min^2)');
+    end
 end
 
 function plotFixationMap(ax, fixationMap, fixationMapSupportX, fixationMapSupportY, ...
@@ -149,9 +161,10 @@ function plotTrajectories(ax, fixEMobj, emPosRange, maxDurationSecondsForFixatio
     hold on;
     xPosArcMin = squeeze(fixEMobj.emPosArcMin(1, :, 1));
     yPosArcMin = squeeze(fixEMobj.emPosArcMin(1, :, 2));
+    plot(fixEMobj.timeAxis*1000, xPosArcMin, '-', 'Color', [1 0.5 0.5], 'LineWidth', 3);
     plot(fixEMobj.timeAxis*1000, xPosArcMin, '-', 'Color', [1 0 0], 'LineWidth', 1.5);
+    plot(fixEMobj.timeAxis*1000, yPosArcMin, '-', 'Color', [0.5 0.5 1], 'LineWidth', 3);
     plot(fixEMobj.timeAxis*1000, yPosArcMin, '-', 'Color', [0 0 1], 'LineWidth', 1.5);
-    
     plot(fixEMobj.timeAxis*1000, yPosArcMin * 0, 'k-');
     hold off;
     grid on;
