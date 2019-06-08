4ac73a0359f245959c0bccfb2ef7b1c247335156
Nicolas Cottaris <cottaris@sas.upenn.edu>
2019-06-08 11:13:58 -0400

    Updates
    Signed-off-by: Nicolas Cottaris <cottaris@sas.upenn.edu>

---
+++ sideprojects/RucciSimulations/computeDiscriminabilityContrastThreshold.m
@@ -0,0 +1,4 @@
+function computeDiscriminabilityContrastThreshold(stimDescriptor, signalName, responseStandardOriStimulus, responseOrthogonalOriStimulus, timeAxis, contrastLevels, figNo)
+    visualizeEnergyResponses(stimDescriptor, signalName, responseStandardOriStimulus, responseOrthogonalOriStimulus, contrastLevels, timeAxis, figNo);
+        
+end
