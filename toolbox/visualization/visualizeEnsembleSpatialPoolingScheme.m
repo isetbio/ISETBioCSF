c91359c6dadae0829538e670215c240988e153f7
Nicolas Cottaris <cottaris@upenn.edu>
2019-09-27 15:45:29 +0300

    Improved pooling visualization

--- toolbox/visualization/visualizeEnsembleSpatialPoolingScheme.m
+++ toolbox/visualization/visualizeEnsembleSpatialPoolingScheme.m
@@ -9,8 +9,7 @@ function hFigs = visualizeEnsembleSpatialPoolingScheme(xaxis, yaxis, spatialModu
     
     unitsNum = numel(V1filterEnsemble);
     hFigs = [];
-    figure();
-    
+   
     
     envelopePoolingWeights = [];
     
@@ -27,19 +26,22 @@ function hFigs = visualizeEnsembleSpatialPoolingScheme(xaxis, yaxis, spatialModu
         if (bandwidthIndex > bandwidthIndices)
             bandwidthIndices = bandwidthIndex;
         end
-         if (orientationIndex > orientationIndices)
+        if (orientationIndex > orientationIndices)
             orientationIndices = orientationIndex;
-         end
+        end
         
         ft2DIndex = theSpatialPoolingFilter.ft2Dindex;
         ft2DindexList(ft2DIndex,:) = [bandwidthIndex orientationIndex];
         
+
         if (isempty(envelopePoolingWeights))
             envelopePoolingWeights = zeros(100,numel(theSpatialPoolingFilter.envelopePoolingWeights));
             envelopePoolingWeights(ft2DIndex,:) = theSpatialPoolingFilter.envelopePoolingWeights;
         else
             envelopePoolingWeights(ft2DIndex,:) = envelopePoolingWeights(ft2DIndex,:) + theSpatialPoolingFilter.envelopePoolingWeights;
         end
+        
+        envelopeProfiles(unitIndex,:,:) = theSpatialPoolingFilter.RFprofile;
     end
     envelopePoolingWeights = envelopePoolingWeights(1:ft2DIndex,:);
     maxWeights = max(envelopePoolingWeights(:));
@@ -54,23 +56,38 @@ function hFigs = visualizeEnsembleSpatialPoolingScheme(xaxis, yaxis, spatialModu
             'bottomMargin',   0.001, ...
             'topMargin',      0.001);
         
-%     hFigEnvelopes = figure(1000); clf;
-%     set(hFigEnvelopes, 'Position', [10 10 1000 1000], 'Color', [1 1 1]);
-%     
-%     for ft2DIndex = 1:size(envelopePoolingWeights,1)
-%         r = squeeze(ft2DindexList(ft2DIndex,:));
-%         subplot('Position', subplotPosVectors(r(2),r(1)).v);
-%         weights = squeeze(envelopePoolingWeights(ft2DIndex,:));
-%         hold on;
-%         plotQuantizedWeights(gca, weights/maxWeights, quantizationLevels, coneLocsDegs, coneX, coneY);
-%         plot(gca,[xaxis(1) xaxis(end)], [0 0 ], 'k-', 'LineWidth', 1.0);
-%         plot(gca,[0 0],[yaxis(1) yaxis(end)], 'k-', 'LineWidth', 1.0);
-%         axis 'image'; axis 'xy';  box 'on'
-%         set(gca, 'Color', [0.5 0.5 0.5]);
-%         set(gca, 'CLim', [0 1], 'XLim', [xaxis(1) xaxis(end)], 'YLim', [yaxis(1) yaxis(end)]);
-%         set(gca, 'XTickLabel', {}, 'YTickLabel', {});
-%     end
-%     drawnow;
+    hFigEnvelopes = figure(1000); clf;
+    set(hFigEnvelopes, 'Position', [10 10 1000 1000], 'Color', [1 1 1]);
+    
+    [X,Y] = meshgrid(xaxis-max(abs(coneApertureOutline.x(:)))/2, yaxis-max(abs(coneApertureOutline.y(:)))/2);
+    for unitIndex = 1:unitsNum 
+        if (unitIndex == 1)
+            theSpatialPoolingFilter = V1filterEnsemble{unitIndex};
+            quantizedWeights = theSpatialPoolingFilter.cosPhasePoolingWeights;
+            maxWeight = max(quantizedWeights(:));
+            plotQuantizedWeights(gca, quantizedWeights/maxWeight, quantizationLevels, coneLocsDegs, coneApertureOutline); hold on;
+            colormap(gca, cMapForWeights);
+        end
+        
+        theProfile = squeeze(envelopeProfiles(unitIndex,:,:));
+        if (unitIndex == 1)
+            [~,c] = contour(gca,X,Y,theProfile, 0.05*[1 1], 'k-');
+        else
+            [~,c] = contour(gca,X,Y,theProfile, 0.05*[1 1], 'k-', 'Color', [0.3 0.3 0.3]);
+        end
+        c.LineWidth = 2.0;
+
+        
+        if (ft2DIndex == unitsNum)
+            plot(gca,[xaxis(1) xaxis(end)], [0 0 ], 'k-', 'LineWidth', 1.0);
+            plot(gca,[0 0],[yaxis(1) yaxis(end)], 'k-', 'LineWidth', 1.0);
+        end
+        axis 'image'; axis 'xy';  box 'on'
+        set(gca, 'Color', [1 1 1]);
+        set(gca, 'CLim', [0 1], 'XLim', [xaxis(1) xaxis(end)], 'YLim', [yaxis(1) yaxis(end)]);
+        set(gca, 'XTickLabel', {}, 'YTickLabel', {});
+    end
+    drawnow;
     
 
     for unitIndex = 1:unitsNum 
@@ -81,14 +98,10 @@ function hFigs = visualizeEnsembleSpatialPoolingScheme(xaxis, yaxis, spatialModu
        
        if ((abs(theSpatialPoolingFilter.rowColPosition(1)) > displayedHalfRows) || ...
            (abs(theSpatialPoolingFilter.rowColPosition(2)) > displayedHalfCols))
-        continue;
+            continue;
        end
        
        if (numel(hFigs) < ft2DIndex)
-            colormap(gray(1024));
-            drawnow;
-            rows = theSpatialPoolingFilter.rowsNum;
-            cols = theSpatialPoolingFilter.colsNum;
     
             subplotPosVectors = NicePlot.getSubPlotPosVectors(...
             'rowsNum', 2*displayedHalfRows+1, ...
@@ -105,12 +118,14 @@ function hFigs = visualizeEnsembleSpatialPoolingScheme(xaxis, yaxis, spatialModu
             %set(gcf,'renderer','opengl');
        end
        
-       maxWeight = max([max(abs(theSpatialPoolingFilter.cosPhasePoolingWeights(:))) max(abs(theSpatialPoolingFilter.sinPhasePoolingWeights(:)))]);
-       quantizedWeights = theSpatialPoolingFilter.cosPhasePoolingWeights;
-       desiredProfile = theSpatialPoolingFilter.cosPhasePoolingProfile;
+       
        row = theSpatialPoolingFilter.rowColPosition(1) + displayedHalfRows+1;
        col = theSpatialPoolingFilter.rowColPosition(2) + displayedHalfCols+1;
        subplot('Position', subplotPosVectors(displayedHalfRows*2+1+1-row,col).v);
+       
+       maxWeight = max([max(abs(theSpatialPoolingFilter.cosPhasePoolingWeights(:))) max(abs(theSpatialPoolingFilter.sinPhasePoolingWeights(:)))]);
+       quantizedWeights = theSpatialPoolingFilter.cosPhasePoolingWeights;
+       desiredProfile = theSpatialPoolingFilter.cosPhasePoolingProfile;
        
        plotQuantizedWeights(gca, quantizedWeights/maxWeight, quantizationLevels, coneLocsDegs, coneApertureOutline);
        hold (gca, 'on')
@@ -122,12 +137,9 @@ function hFigs = visualizeEnsembleSpatialPoolingScheme(xaxis, yaxis, spatialModu
        set(gca, 'Color', [1 1 1]);
        set(gca, 'CLim', [0 1], 'XLim', [xaxis(1) xaxis(end)], 'YLim', [yaxis(1) yaxis(end)]);
        set(gca, 'XTickLabel', {}, 'YTickLabel', {});
-    end % 
-    
-    
-    
+    end %
     drawnow;
     
-%    hFigs(numel(hFigs)+1) = hFigEnvelopes;
+    hFigs(numel(hFigs)+1) = hFigEnvelopes;
 end
 
